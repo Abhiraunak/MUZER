1968f1fbc639b26ea28aab474b34c1b342675bc7
Abhishek <abhishekkp1721@hotmail.com>
2025-01-25 17:53:55 +0530

    add app/api/streams/my/page.ts

---
+++ app/api/streams/my/page.ts
@@ -0,0 +1,45 @@
+import { prismaClient } from "../../lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest){
+    const session = await getServerSession();
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    });
+
+    if (!user) {
+        return NextResponse.json({
+            message: "unauthenticates"
+        }, {
+            status: 403
+        })
+    }
+
+    const streams = await prismaClient.stream.findMany({
+        where: {
+            userId: user.id ?? ""
+        }, 
+
+        include: {
+            _count: {
+                select: {
+                    upvotes : true
+                }
+            }
+        }
+    })
+
+  
+    return NextResponse.json({
+        streams: streams.map(({ _count, ...rest }) => ({
+            ...rest,
+            upvotes: _count?.upvotes || 0 // Defaulting to 0 if _count or upvotes is undefined
+        }))
+    });
+    
+
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard(){
+    return (
+        <div>
+            hi from dashboard
+        </div>
+    )
+}
